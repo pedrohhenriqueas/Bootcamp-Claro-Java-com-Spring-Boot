dcd0cf52b67701c49e601d8e1be99ba8c4acd777
Pedro Henrique <144642781+pedrohhenriqueas@users.noreply.github.com>
2024-08-06 20:56:36 -0300

    Add files via upload

---
+++ BancoDigitalDIO/src/Conta.java
@@ -0,0 +1,73 @@
+
+public abstract class Conta implements IConta {
+	
+	private static final int AGENCIA_PADRAO = 1;
+	private static int SEQUENCIAL = 1;
+
+	protected int agencia;
+	protected int numero;
+	protected double saldo;
+	protected Cliente cliente;
+	protected double saldoCorretora;
+
+	public Conta(Cliente cliente) {
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.numero = SEQUENCIAL++;
+		this.cliente = cliente;
+		this.saldoCorretora = saldoCorretora;
+	}
+
+	@Override
+	public void sacar(double valor) {
+		saldo -= valor;
+	}
+
+	@Override
+	public void depositar(double valor) {
+		saldo += valor;
+	}
+
+	@Override
+	public void transferir(double valor, IConta contaDestino) {
+		this.sacar(valor);
+		contaDestino.depositar(valor);
+	}
+	
+	public void investir(double valor) {
+		saldo -= valor;
+		saldoCorretora += valor;
+	}
+	
+	public void retirarInvestimento(double valor) {
+		saldoCorretora -= valor;
+		saldo += valor;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public double getSaldoCorretora() {
+		return saldoCorretora;
+	}
+
+	public void setSaldoCorretora(double saldoCorretora) {
+		this.saldoCorretora = saldoCorretora;
+	}
+
+	protected void imprimirInfosComuns() {
+		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+		System.out.println(String.format("Agencia: %d", this.agencia));
+		System.out.println(String.format("Numero: %d", this.numero));
+		System.out.println(String.format("Saldo: %.2f", this.saldo));
+		System.out.println(String.format("Saldo em investimentos: %.2f", this.saldoCorretora));
+	}
+}
